0ba67dd3f241597c438fc55fbd59f9c5d1733b05
Jessica Reid <jess.reid1992@outlook.com>
2024-12-03 10:02:15 +0000

    Adds missing type hints

--- tst/test_day_1.py
+++ tst/test_day_1.py
@@ -3,19 +3,19 @@ from utils import read_all_of_file
 
 
 def test_part_1_example():
-    locations = read_all_of_file("day_1_sample.txt")
+    locations: list[str]= read_all_of_file("day_1_sample.txt")
     assert day1.part_1(locations) == 11
 
 
 def test_part_1():
-    locations = read_all_of_file("day_1.txt")
+    locations: list[str]= read_all_of_file("day_1.txt")
     assert day1.part_1(locations) == 1765812
 
 
 def test_part_2_examples():
-    locations = read_all_of_file("day_1_sample.txt")
+    locations: list[str]= read_all_of_file("day_1_sample.txt")
     assert day1.part_2(locations) == 31
 
 def test_part_2():
-    locations = read_all_of_file("day_1.txt")
+    locations: list[str]= read_all_of_file("day_1.txt")
     assert day1.part_2(locations) == 20520794
--- tst/test_day_2.py
+++ tst/test_day_2.py
@@ -3,20 +3,20 @@ from utils import read_all_of_file
 
 
 def test_part_1_example():
-    locations = read_all_of_file("day_2_sample.txt")
+    locations: list[str]= read_all_of_file("day_2_sample.txt")
     assert day2.part_1(locations) == 2
 
 
 def test_part_1():
-    locations = read_all_of_file("day_2.txt")
+    locations: list[str]= read_all_of_file("day_2.txt")
     assert day2.part_1(locations) == 383
 
 
 def test_part_2_examples():
-    locations = read_all_of_file("day_2_sample.txt")
+    locations: list[str]= read_all_of_file("day_2_sample.txt")
     assert day2.part_2(locations) == 4
 
 
 def test_part_2():
-    locations = read_all_of_file("day_2.txt")
+    locations: list[str]= read_all_of_file("day_2.txt")
     assert day2.part_2(locations) == 436
