46039e3b059559af599e0152dfb4a9a3ad525420
Jessica Reid <jess.reid1992@outlook.com>
2024-12-04 16:00:53 +0000

    Extracts common list manipulation to method

--- src/day4.py
+++ src/day4.py
@@ -8,20 +8,19 @@ def part_1(word_search: list[str]) -> int:
 
 def part_2(word_search: list[str]) -> int:
     total: int = 0
-    print()
     for x in range(len(word_search[0]) - 2):
         for y in range(len(word_search) - 2):
-            south_east: str = word_search[y][x] + word_search[y+1][x+1] + word_search[y+2][x+2]
-            north_east: str = word_search[y+2][x] + word_search[y+1][x+1] + word_search[y][x+2]
-            south_west: str = word_search[y][x+2] + word_search[y+1][x+1] + word_search[y+2][x]
-            north_west: str = word_search[y+2][x+2] + word_search[y+1][x+1] + word_search[y][x]
+            south_east: str = word_search[y][x] + word_search[y + 1][x + 1] + word_search[y + 2][x + 2]
+            north_east: str = word_search[y + 2][x] + word_search[y + 1][x + 1] + word_search[y][x + 2]
+            south_west: str = word_search[y][x + 2] + word_search[y + 1][x + 1] + word_search[y + 2][x]
+            north_west: str = word_search[y + 2][x + 2] + word_search[y + 1][x + 1] + word_search[y][x]
             if south_east == PART_2_TARGET and north_east == PART_2_TARGET:
                 total += 1
-            if north_west == PART_2_TARGET and north_east == PART_2_TARGET:
+            elif north_west == PART_2_TARGET and north_east == PART_2_TARGET:
                 total += 1
-            if south_east == PART_2_TARGET and south_west == PART_2_TARGET:
+            elif south_east == PART_2_TARGET and south_west == PART_2_TARGET:
                 total += 1
-            if north_west == PART_2_TARGET and south_west == PART_2_TARGET:
+            elif north_west == PART_2_TARGET and south_west == PART_2_TARGET:
                 total += 1
     return total
 
@@ -31,25 +30,22 @@ def horizontal_matches(word_search: list[str]) -> int:
 
 
 def vertical_matches(word_search: list[str]) -> int:
-    inverted: list[str] = []
-    for char_num in range(len(word_search[0])):
-        vertical: str = ""
-        for line in word_search:
-            vertical += line[char_num]
-        inverted.append(vertical)
+    inverted: list[str] = manipulate(word_search, lambda x, y: x)
     return horizontal_matches(inverted)
 
 
 def diagonal_matches(word_search: list[str]) -> int:
-    forward_diagonals: list[str] = ["" for _ in range(len(word_search) * 2 - 1)]
-    for x in range(len(word_search[0])):
-        for y in range(len(word_search)):
-            forward_diagonals[x + y] += word_search[y][x]
+    forward_diagonals: list[str] = manipulate(word_search, lambda x, y: x + y)
     forward_matches: int = horizontal_matches(forward_diagonals)
 
-    backward_diagonals: list[str] = ["" for _ in range(len(word_search) * 2 - 1)]
-    for x in range(len(word_search[0])):
-        for y in range(len(word_search)):
-            backward_diagonals[x - y] += word_search[y][x]
+    backward_diagonals: list[str] = manipulate(word_search, lambda x, y: x - y)
     backward_matches = horizontal_matches(backward_diagonals)
     return forward_matches + backward_matches
+
+
+def manipulate(word_search: list[str], index_func) -> list[str]:
+    output_list: list[str] = ["" for _ in range(len(word_search) * 2 - 1)]
+    for x in range(len(word_search[0])):
+        for y in range(len(word_search)):
+            output_list[index_func(x, y)] += word_search[y][x]
+    return output_list
