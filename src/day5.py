ff2c092a7c2d57493c9010ec2ed7f554b0e6c190
Jessica Reid <jess.reid1992@outlook.com>
2024-12-05 09:46:46 +0000

    Day 5 simplification between parts
    If part 2 is the sum of the middle pages for rows fixed by sorting, then part 1 is the sum of the middle pages for rows _not_ changed by sorting.

--- src/day5.py
+++ src/day5.py
@@ -6,13 +6,19 @@ from string_utils import string_to_list_ints
 
 def part_1(page_numbers: list[str]) -> int:
     orderings, updates = parse_input(page_numbers)
-    return sum([update[len(update) // 2] for update in updates if is_correctly_ordered(update, orderings)])
+    return sum([update[len(update) // 2] for update in updates
+                if update == fix_update(update, orderings)])
 
 
 def part_2(page_numbers: list[str]) -> int:
     orderings, updates = parse_input(page_numbers)
-    fixed_updates = [fix_update(update, orderings) for update in updates if not is_correctly_ordered(update, orderings)]
-    return sum([update[len(update) // 2] for update in fixed_updates])
+    return sum([fixed_update[len(fixed_update) // 2] for update in updates
+                if update != (fixed_update := fix_update(update, orderings))])
+
+
+def fix_update(update: list[int], orderings: dict[int, set[int]]) -> list[int]:
+    # if left page needs to be after right page in orderings, return 1 in comparator else return -1
+    return sorted(update, key=cmp_to_key(lambda left, right: 1 if left in orderings[right] else -1))
 
 
 def parse_input(page_numbers: list[str]):
@@ -28,16 +34,3 @@ def parse_input(page_numbers: list[str]):
             page_pair: list[str] = page_numbers[i].split("|")
             orderings[int(page_pair[0])].add(int(page_pair[1]))
     return orderings, updates
-
-
-def fix_update(update: list[int], orderings: dict[int, set[int]]) -> list[int]:
-    # if left page needs to be after right page in orderings, return 1 in comparator else return -1
-    return sorted(update, key=cmp_to_key(lambda left, right: 1 if left in orderings[right] else -1))
-
-
-def is_correctly_ordered(update: list[int], orderings: dict[int, set[int]]) -> bool:
-    for i in range(len(update)):
-        for j in range(i + 1, len(update)):
-            if update[i] in orderings[update[j]]:
-                return False # page pair needs to be in opposite order
-    return True
